6fefa2852715f435b3394ef9d091447e39aa6e92
Michael Hennerich <michael.hennerich@analog.com>
2015-08-19 22:51:10 +0200

    src/CMSIS/vectors_ADuCM360: Implement vector table properly once
    so we never have to touch this file again
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- projects/ADuCM360_demo_blink/system/src/CMSIS/vectors_ADuCM360.c
+++ projects/ADuCM360_demo_blink/system/src/CMSIS/vectors_ADuCM360.c
@@ -17,31 +17,47 @@ Default_Handler(void);
 // defines a handler (with the same name), this will automatically take
 // precedence over these weak definitions
 //
-// TODO: Rename this and add the actual routines here.
 
-//void __attribute__ ((weak, alias ("Default_Handler"))) WakeUp_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int0_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int1_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int2_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int3_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int4_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int5_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int6_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int7_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) Watchdog_Int_Handler(void);
-void __attribute__ ((weak, alias ("Default_Handler"))) GP_Tmr0_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) GP_Tmr1_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) ADC0_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) ADC1_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) SINC2_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) UART_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) SPI0_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) SPI1_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) I2C0_Slave_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) PWMTRIP_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) PWM0_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) PWM1_Int_Handler(void);
-//void __attribute__ ((weak, alias ("Default_Handler"))) PWM2_Int_Handler(void);
+void __attribute__ ((weak, alias ("Default_Handler"))) WakeUp_Int_Handler(void);        //[ 0]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int0_Handler(void);          //[ 1]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int1_Handler(void);          //[ 2]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int2_Handler(void);          //[ 3]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int3_Handler(void);          //[ 4]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int4_Handler(void);          //[ 5]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int5_Handler(void);          //[ 6]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int6_Handler(void);          //[ 7]
+void __attribute__ ((weak, alias ("Default_Handler"))) Ext_Int7_Handler(void);          //[ 8]
+void __attribute__ ((weak, alias ("Default_Handler"))) WDog_Tmr_Int_Handler(void);      //[ 9]
+void __attribute__ ((weak, alias ("Default_Handler"))) GP_Tmr0_Int_Handler(void);       //[11]
+void __attribute__ ((weak, alias ("Default_Handler"))) GP_Tmr1_Int_Handler(void);       //[12]
+void __attribute__ ((weak, alias ("Default_Handler"))) ADC0_Int_Handler(void);          //[13]
+void __attribute__ ((weak, alias ("Default_Handler"))) ADC1_Int_Handler(void);          //[14]
+void __attribute__ ((weak, alias ("Default_Handler"))) SINC2_Int_Handler(void);         //[15]
+void __attribute__ ((weak, alias ("Default_Handler"))) Flsh_Int_Handler(void);          //[16]
+void __attribute__ ((weak, alias ("Default_Handler"))) UART_Int_Handler(void);          //[17]
+void __attribute__ ((weak, alias ("Default_Handler"))) SPI0_Int_Handler(void);          //[18]
+void __attribute__ ((weak, alias ("Default_Handler"))) SPI1_Int_Handler(void);          //[19]
+void __attribute__ ((weak, alias ("Default_Handler"))) I2C0_Slave_Int_Handler(void);    //[20]
+void __attribute__ ((weak, alias ("Default_Handler"))) I2C0_Master_Int_Handler(void);   //[21]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_Err_Int_Handler(void);       //[22]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_SPI1_TX_Int_Handler(void);   //[23]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_SPI1_RX_Int_Handler(void);   //[24]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_UART_TX_Int_Handler(void);   //[25]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_UART_RX_Int_Handler(void);   //[26]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_I2C0_STX_Int_Handler(void);  //[27]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_I2C0_SRX_Int_Handler(void);  //[28]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_I2C0_MTX_Int_Handler(void);  //[29]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_I2C0_MRX_Int_Handler(void);  //[30]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_DAC_Out_Int_Handler(void);   //[31]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_ADC0_Int_Handler(void);      //[32]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_ADC1_Int_Handler(void);      //[33]
+void __attribute__ ((weak, alias ("Default_Handler"))) DMA_SINC2_Int_Handler(void);     //[34]
+void __attribute__ ((weak, alias ("Default_Handler"))) PWMTRIP_Int_Handler(void);       //[35]
+void __attribute__ ((weak, alias ("Default_Handler"))) PWM0_Int_Handler(void);          //[36]
+void __attribute__ ((weak, alias ("Default_Handler"))) PWM1_Int_Handler(void);          //[37]
+void __attribute__ ((weak, alias ("Default_Handler"))) PWM2_Int_Handler(void);          //[38]
+
+
 
 // ----------------------------------------------------------------------------
 
@@ -87,38 +103,46 @@ pHandler __isr_vectors[] =
 
         // ----------------------------------------------------------------------
         // ADuCM360 vectors
-        Default_Handler,                  		  // [00] WakeUp_Int_Handler
-        Default_Handler,                  		  // [01]
-        Default_Handler,                  		  // [02]
-        Default_Handler,                  		  // [03]
-        Default_Handler,                  		  // [04]
-        Default_Handler,                  		  // [05]
-        Default_Handler,                  		  // [06]
-        Default_Handler,                  		  // [07]
-        Default_Handler,                  		  // [08]
-        Default_Handler,                  		  // [09]
-        Default_Handler,                  		  // [10]
-		GP_Tmr0_Int_Handler,               		  // [11] //GP_Tmr0_Int_Handler
-        Default_Handler,                  		  // [12]
-        Default_Handler,                  		  // [13]
-        Default_Handler,                  		  // [14] ADC 1
-        Default_Handler,                  		  // [15]
-        Default_Handler,                  		  // [16]
-        Default_Handler,                  		  // [17]
-        Default_Handler,                  		  // [18]
-        Default_Handler,                  		  // [19]
-        Default_Handler,                  		  // [20]
-        Default_Handler,               		  	  // [21]
-        Default_Handler,                  		  // [22]
-        Default_Handler,                  		  // [23]
-        Default_Handler,                  		  // [24]
-        Default_Handler,                  		  // [25]
-        Default_Handler,                  		  // [26]
-        Default_Handler,                  		  // [27]
-        Default_Handler,                  		  // [28]
-        Default_Handler,                  		  // [29]
-
-    // TODO: rename and add more vectors here
+		WakeUp_Int_Handler,        //[ 0]
+		Ext_Int0_Handler,          //[ 1]
+		Ext_Int1_Handler,          //[ 2]
+		Ext_Int2_Handler,          //[ 3]
+		Ext_Int3_Handler,          //[ 4]
+		Ext_Int4_Handler,          //[ 5]
+		Ext_Int5_Handler,          //[ 6]
+		Ext_Int6_Handler,          //[ 7]
+		Ext_Int7_Handler,          //[ 8]
+		WDog_Tmr_Int_Handler,      //[ 9]
+		0,                         //[10]
+		GP_Tmr0_Int_Handler,       //[11]
+		GP_Tmr1_Int_Handler,       //[12]
+		ADC0_Int_Handler,          //[13]
+		ADC1_Int_Handler,          //[14]
+		SINC2_Int_Handler,         //[15]
+		Flsh_Int_Handler,          //[16]
+		UART_Int_Handler,          //[17]
+		SPI0_Int_Handler,          //[18]
+		SPI1_Int_Handler,          //[19]
+		I2C0_Slave_Int_Handler,    //[20]
+		I2C0_Master_Int_Handler,   //[21]
+		DMA_Err_Int_Handler,       //[22]
+		DMA_SPI1_TX_Int_Handler,   //[23]
+		DMA_SPI1_RX_Int_Handler,   //[24]
+		DMA_UART_TX_Int_Handler,   //[25]
+		DMA_UART_RX_Int_Handler,   //[26]
+		DMA_I2C0_STX_Int_Handler,  //[27]
+		DMA_I2C0_SRX_Int_Handler,  //[28]
+		DMA_I2C0_MTX_Int_Handler,  //[29]
+		DMA_I2C0_MRX_Int_Handler,  //[30]
+		DMA_DAC_Out_Int_Handler,   //[31]
+		DMA_ADC0_Int_Handler,      //[32]
+		DMA_ADC1_Int_Handler,      //[33]
+		DMA_SINC2_Int_Handler,     //[34]
+		PWMTRIP_Int_Handler,       //[35]
+		PWM0_Int_Handler,          //[36]
+		PWM1_Int_Handler,          //[37]
+		PWM2_Int_Handler,          //[38]
+		0,                         //[39]
     };
 
 // ----------------------------------------------------------------------------
