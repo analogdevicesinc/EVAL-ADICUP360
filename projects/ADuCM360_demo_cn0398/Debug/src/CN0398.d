ddcec8ae876db97f51ac71c4f6b546e8459e4216
vlupei <Veronica.Lupei@analog.com>
2016-10-24 17:42:15 +0300

    ADuCM360_demo_cn0398: Initial version.

---
+++ projects/ADuCM360_demo_cn0398/Debug/src/CN0398.d
@@ -0,0 +1,40 @@
+src/CN0398.o: ../src/CN0398.cpp ../include/CN0398.h ../include/AD7124.h \
+ ../include/AD7124.h ../include/Communication.h \
+ ../system/include/cmsis/ADuCM360.h ../system/include/cmsis/core_cm3.h \
+ ../system/include/cmsis/core_cmInstr.h \
+ ../system/include/cmsis/cmsis_gcc.h \
+ ../system/include/cmsis/core_cmFunc.h \
+ ../system/include/cmsis/system_ADuCM360.h \
+ ../system/include/aducm360/SpiLib.h ../system/include/aducm360/DioLib.h \
+ ../include/Timer.h ../system/include/cmsis/cmsis_device.h \
+ ../system/include/cmsis/ADuCM360.h
+
+../include/CN0398.h:
+
+../include/AD7124.h:
+
+../include/AD7124.h:
+
+../include/Communication.h:
+
+../system/include/cmsis/ADuCM360.h:
+
+../system/include/cmsis/core_cm3.h:
+
+../system/include/cmsis/core_cmInstr.h:
+
+../system/include/cmsis/cmsis_gcc.h:
+
+../system/include/cmsis/core_cmFunc.h:
+
+../system/include/cmsis/system_ADuCM360.h:
+
+../system/include/aducm360/SpiLib.h:
+
+../system/include/aducm360/DioLib.h:
+
+../include/Timer.h:
+
+../system/include/cmsis/cmsis_device.h:
+
+../system/include/cmsis/ADuCM360.h:
---
+++ projects/ADuCM360_demo_cn0398/Debug/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/AD7124.cpp \
+../src/CN0398.cpp \
+../src/Communication.cpp \
+../src/Timer.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/AD7124.o \
+./src/CN0398.o \
+./src/Communication.o \
+./src/Timer.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/AD7124.d \
+./src/CN0398.d \
+./src/Communication.d \
+./src/Timer.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C++ Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m3 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DTRACE -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/aducm360" -std=gnu++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
